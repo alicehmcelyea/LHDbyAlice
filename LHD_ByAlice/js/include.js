4d3147ff443926df4381b167cd35e2fed61d091c
Alice McElyea <124388981+alicehmcelyea@users.noreply.github.com>
2025-04-16 16:20:43 -0700

    Add files via upload

---
+++ LHD_ByAlice/js/include.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const includes = document.querySelectorAll('[data-include]');
+    includes.forEach(el => {
+      const file = el.getAttribute('data-include');
+      fetch(file)
+        .then(res => res.text())
+        .then(data => {
+          el.innerHTML = data;
+        });
+    });
+  });
+  
